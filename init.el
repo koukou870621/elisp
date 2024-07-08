b6a13707ced88eda9a828d4d820108e46c606abc
guozimao <z.guo@gwell.co.jp>
2024-07-08 10:00:43 +0900

    commit

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 (add-to-list 'load-path "~/mylisp")
 ;(add-to-list 'load-path "~/mylisp/lib")
 
-;(load "node99.el")
+;(load "node99.el")x
 (load "java99.el")
 (load "python99.el")
 (load "common.el")
