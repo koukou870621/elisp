e4a51e8c7706933ac6c79b4a84eb372fc3a97cef
guozimao <z.guo@gwell.co.jp>
2024-07-18 18:09:05 +0900

    comit

--- node99.el
+++ node99.el
@@ -1,51 +1,34 @@
 
-;; (require 'package)
-;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; (package-initialize)
-;; (unless package-archive-contents (package-refresh-contents))
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-initialize 'use-package))
 
-;; (setq package-list '(dap-mode typescript-mode tree-sitter tree-sitter-langs lsp-mode lsp-ui))
-;; (dolist (package package-list)
-;;   (unless (package-installed-p package) (package-install package)))
+(use-package company
+  :ensure t
+  :config
+  (add-hook 'after-init-hook 'global-company-mode))
 
+(use-package tern
+  :ensure t
+  :hook (js-mode . tern-mode)
+  :config
+  (add-hook 'js2-mode-hook (lambda () (tern-mode t)))
+  (add-hook 'web-mode-hook (lambda () (tern-mode t))))
 
-;; (require 'tree-sitter-langs)
-;; (require 'tree-sitter)
-;; (global-tree-sitter-mode)
-;; (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
+;; (use-package company-tern
+;;   :load-path "./lib/company-tern"
+;;   :after (company tern)
+;;   :config
+;;   (add-to-list 'company-backends 'company-tern))
 
-;; (require 'lsp-mode)
-;; (add-hook 'typescript-mode-hook 'lsp-deferred)
-;; (add-hook 'javascript-mode-hook 'lsp-deferred)
+(add-to-list 'load-path "./lib/company-tern")
 
-;; (defun my-setup-dap-node ()
-;;   (require 'dap-node)
-;;   (unless (file-exists-p dap-node-debug-path)(dap-node-setup)))
-;; (add-hook 'typescript-mode-hook 'my-setup-dap-node)
-;; (add-hook 'javascript-mode-hook 'my-setup-dap-node)
-
-;; (require 'dap-node)
-;; (if (file-exists-p (nth 1 dap-node-debug-program))
-;;     (message "NodeJs debugger successfully installed")
-;;   (message "NodeJs debugger install failed. Please download it manually"))
-
-;; (add-hook 'js2-mode-hook 'ac-js2-mode)
-;; (add-to-list 'company-backends 'ac-js2-company)
-
-;; (setq ac-js2-evaluate-calls t)
-;; (setq ac-js2-external-libraries '("full/path/to/a-library.js"))
-
-
-(add-to-list 'load-path "~/.emacs.d/company-tern")
 (require 'company-tern)
-
-
-(require 'company)
-(add-hook 'after-init-hook 'global-company-mode)
-
+;(autoload 'tern-mode "company-tern.el" nil t)
 (add-to-list 'company-backends 'company-tern)
-(add-hook 'js2-mode-hook (lambda () (tern-mode t)))
 
+;; (define-key tern-mode-keymap (kdb "M-.") nil)
+;; (define-key tern-mode-keymap (kbd "M-,") nil)
 
 
 (provide 'node99)
