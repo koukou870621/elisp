883b26163140b3a6f352b861ab636d5b613cbbe7
guozimao <z.guo@gwell.co.jp>
2024-07-25 13:49:18 +0900

    comit

---
+++ lib/snake-game/a.el
@@ -0,0 +1,15 @@
+	
+	
+	
+	
+	  		
+       	  		
+(defun draw-rectangle-example()	      
+  "draw a rectangle in picture-mode." 
+  (interactive)			      
+; (picture-mode)		      
+  (picture-draw-rectangle 5 10))      
+			      	      
+				      
+(draw-rectangle-example)	     
+
