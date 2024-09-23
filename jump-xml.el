6dc0d2fbe3329e4c5953fab23eb19c73df35cf36
コウコウ <z.guo@gwell.co.jp>
2024-09-23 14:36:39 +0900

    commit

--- jump-xml.el
+++ jump-xml.el
@@ -61,4 +61,74 @@
          (string-trim (shell-command-to-string "wl-paste"))))
     (insert (decode-coding-string clipboard-content 'utf-8))))
 
+
+(defun copy-jsp-to-tomcat-webapps (orig-fun &rest args)
+  "copy jsp to tomcat webapps"
+  (message "--copy-jsp-to-tomcat-webapps --")
+  (setq suffix (get-current-file-extension))
+  (if (string-equal suffix "jsp")
+      (progn
+        (setq current-project-name (get-current-project-name))
+        (setq tomcat-home "/home/huanghao/soft/apache-tomcat-7.0.79")
+        (setq webapps (concat tomcat-home "/webapps"))
+        (setq current-file-path (buffer-file-name))
+        (setq tomcat-project-path
+              (concat webapps "/" current-project-name))
+        (message tomcat-project-path)
+	(setq dist-path (concat
+			 tomcat-project-path "/" (get-path-after-web-root current-file-path)))
+	(message (concat "current-file-path:" current-file-path))
+	(message (concat "dist-path:" dist-path))
+        (copy-file current-file-path
+                   dist-path
+                   t)))
+  )
+
+
+(defun get-current-filename ()
+  "Return the name of the current buffer's file."
+  (if buffer-file-name
+      (file-name-nondirectory buffer-file-name)
+    "Buffer is not visiting a file."))
+
+
+(defun get-current-project-name ()
+  "Get the name of the current project."
+  (let ((project (project-current)))
+    (if project
+        (file-name-nondirectory
+         (directory-file-name (project-root project)))
+      (message "Not in a project"))))
+
+
+(defun get-current-file-extension ()
+  "Return the extension of the current buffer's file."
+  (if buffer-file-name
+      (file-name-extension buffer-file-name)
+    "Buffer is not visiting a file."))
+
+
+
+(defun get-path-after-web-inf (path)
+  "Return the substring of PATH starting from 'WEB-INF'."
+  (let ((start (string-match "WEB-INF" path)))
+    (if start
+        (substring path start)
+      "WEB-INF not found in the path.")))
+
+
+(defun get-path-after-web-root (path)
+  "Return the substring of PATH starting from 'WebRoot'."
+  (let ((start (string-match "jsp" path)))
+    (if start
+        (substring path start)
+      "WEB-INF not found in the path.")))
+
+;(get-path-after-web-root "/home/huanghao/java_work/gwclerk/src/main/webapp/WEB-INF/jsp/apply_confirm.jsp")
+;(get-path-after-web-root "/home/huanghao/java_work/gwhozentool/WebRoot/jsp/hozen_syousai.jsp")
+
+;(get-path-after-web-inf "/home/huanghao/java_work/gwclerk/src/main/webapp/WEB-INF/jsp/apply_confirm.jsp")
+
+
+
 (provide 'jump-xml)
